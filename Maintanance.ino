4e3ea8759c004d2346cb37dd722657fb5d52ca95
Simon Christmann <simon@christmann.email>
2017-08-16 19:40:13 +0200

    Add info/description publisher

--- Maintanance.ino
+++ Maintanance.ino
@@ -1,14 +1,24 @@
 Thread uptimeThread = Thread();
+Thread infoThread = Thread();
 
 void setup_Maintanance() {
   uptimeThread.onRun(uptimePublisher);
-  uptimeThread.setInterval(10000);
+  uptimeThread.setInterval(10 * 1000);
   threadControl.add(&uptimeThread);
+
+  infoThread.onRun(infoPublisher);
+  infoThread.setInterval(60 * 1000);
+  threadControl.add(&infoThread);
 }
 
 void uptimePublisher() {
   unsigned long time = millis();
   mqtt_publish(String(UPTIME_TOPIC)+"/ms", String(time) );
   mqtt_publish(String(UPTIME_TOPIC),       String(time/1000) );
+}
+
+void infoPublisher() {
+  mqtt_publish(String(ID_TOPIC), String("ESP")+ESP.getChipId() );
+  mqtt_publish(String(DESCRIPTION_TOPIC), String(DESCRIPTION) );
 }
 
--- default_config.h
+++ default_config.h
@@ -6,7 +6,10 @@
 #define LED_SAT         255
 #define LED_BRIGHT      100
 
-#define UPTIME_TOPIC    "/devices/hifi01/uptime"
+#define UPTIME_TOPIC          "/devices/hifi01/uptime"
+#define DESCRIPTION_TOPIC     "/devices/hifi01/description"
+#define ID_TOPIC              "/devices/hifi01/id"
+#define DESCRIPTION           "DevilRemote Project - WiFi enabled replacement for 'Teufel CC 21 RC'"
 
 #define POWER_TOPIC     "/devices/hifi01/power"
 #define VOLUME_TOPIC    "/devices/hifi01/volume"
