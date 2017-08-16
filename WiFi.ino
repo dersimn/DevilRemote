e8047a100d30c5a298952a49804b5aa0d6f0b28d
Simon Christmann <simon@christmann.email>
2017-08-16 21:30:46 +0200

    Make program work without Wifi connection

--- WiFi.ino
+++ WiFi.ino
@@ -1,11 +1,29 @@
+Thread wifiThread = Thread();
+
 void setup_WiFi() {
   WiFi.softAPdisconnect(true);
   Log.info(String("Connecting to Wifi SSID ") + ssid);
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
+  for (int i = 0; i < 10; i++) {
+    if (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+    }
   }
-  Log.info(String("Connected with IP: ") + WiFi.localIP().toString() );
+  if (WiFi.status() == WL_CONNECTED) {
+    Log.info(String("Connected with IP: ") + WiFi.localIP().toString() );
+  } else {
+    Log.warn("Could NOT connect to WiFi");
+  }
+
+  wifiThread.onRun(wifiFunction);
+  wifiThread.setInterval(60 * 1000);
+  threadControl.add(&wifiThread);
+}
+
+void wifiFunction() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Log.warn("WiFi Connection lost");
+  }
 }
 
